b382a225ef61893ee00abade09f5293338ff567b
LEGEND-DIVYAOP <93478361+LEGEND-GIRL@users.noreply.github.com>
2021-11-29 19:53:32 +0530

    Update LegendString.py

--- LegendString.py
+++ LegendString.py
@@ -15,7 +15,7 @@ if okvai == "69":
         client.send_message("me", client.session.save())
         client.send_message(
             "me",
-            "Above is your #LEGENDBOT STRING SESSION \nPaste this string in Heroku Var.\n\n[Team LegendBot](t.me/Official_LegendBot)",
+            "Above is your #PROUD-INDIAN STRING SESSION \nPaste this string in Heroku Var.\n\n[Team proud indian](t.me/indianuserbot)",
         )
 
 else:
