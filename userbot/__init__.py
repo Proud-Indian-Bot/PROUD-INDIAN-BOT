2a4169f47fac72d2e1bcf4aa6024ca71e28d9d1b
LEGEND-DIVYAOP <93478361+LEGEND-GIRL@users.noreply.github.com>
2021-11-30 00:03:00 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -86,7 +86,7 @@ if bool(ENV):
         basicConfig(
             format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=INFO
         )
-    LOGS = getLogger("[Lêɠêɳ̃dẞø† 3.0]")
+    LOGS = getLogger("[PROUD-INDIAN-BOT 3.0]")
 
 try:
     if Config.HEROKU_API_KEY is not None or Config.HEROKU_APP_NAME is not None:
