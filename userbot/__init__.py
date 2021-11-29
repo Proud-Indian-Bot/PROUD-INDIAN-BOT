43a92440765f92f918b84af17c7a410c1c5ed5c8
LEGEND-DIVYAOP <93478361+LEGEND-GIRL@users.noreply.github.com>
2021-11-29 23:48:12 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -35,7 +35,7 @@ except Exception as e:
     sys.exit()
 
 
-Legend = TelegramClient(
+Legendbot = TelegramClient(
     session="proudindianbot",
     api_id=Config.APP_ID,
     api_hash=Config.API_HASH,
