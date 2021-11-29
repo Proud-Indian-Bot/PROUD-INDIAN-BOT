de0ff389a9cb843482a2fc53eacf61a074cb55b4
LEGEND-DIVYAOP <93478361+LEGEND-GIRL@users.noreply.github.com>
2021-11-29 22:32:44 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -1,1 +1,172 @@
+import os
+import re
+import sys
+from pathlib import Path
 
+import telethon.utils
+from telethon import Button, TelegramClient, custom, events
+from telethon.tl.functions.channels import JoinChannelRequest
+
+from LEGENDBOY import lnbot
+from userbot import LOGS, LEGENDversion, bot
+from userbot.Config import Config
+from userbot.utils import (
+    load_abuse,
+    load_addons,
+    load_module,
+    start_assistant,
+    start_spam,
+)
+from var import Var
+
+l2 = Config.SUDO_COMMAND_HAND_LER
+ZIDDI_PIC = "https://te.legra.ph/file/c9bbbf280ec3bec0c231c.jpg"
+l1 = Config.COMMAND_HAND_LER
+
+
+async def add_bot(bot_token):
+    try:
+        await bot.start(bot_token)
+        bot.me = await bot.get_me()
+        bot.uid = telethon.utils.get_peer_id(bot.me)
+    except Exception as e:
+        print(f"ZIDDI_STRING - {str(e)}")
+        sys.exit()
+
+
+if len(sys.argv) not in (1, 3, 4):
+    bot.disconnect()
+else:
+    bot.tgbot = None
+    try:
+        if Var.BOT_USERNAME is not None:
+            LOGS.info("Checking Telegram Bot Username...")
+            bot.tgbot = TelegramClient(
+                "BOT_TOKEN", api_id=Var.APP_ID, api_hash=Var.API_HASH
+            ).start(bot_token=Var.BOT_TOKEN)
+            LOGS.info("Checking Completed. Proceeding to next step...")
+            LOGS.info("♥️ Starting ziddiqueen ♥️")
+            bot.loop.run_until_complete(add_bot(Config.BOT_USERNAME))
+            LOGS.info("🥇🔥 ziddiqueen Startup Completed 🔥🥇")
+        else:
+            bot.start()
+    except Exception as e:
+        LOGS.error(f"BOT_TOKEN - {str(e)}")
+        sys.exit()
+
+print("📍⚜Loading Modules / Plugins⚜✔")
+
+
+async def module():
+    import glob
+
+    path = "userbot/plugins/*.py"
+    files = glob.glob(path)
+    for name in files:
+        with open(name) as f:
+            path1 = Path(f.name)
+            shortname = path1.stem
+            load_module(shortname.replace(".py", ""))
+
+
+assistant = os.environ.get("ASSISTANT", None)
+
+
+async def assistants():
+    if assistant == "ON":
+        import glob
+
+        path = "userbot/plugins/assistant/*.py"
+        files = glob.glob(path)
+        for name in files:
+            with open(name) as f:
+                path1 = Path(f.name)
+                shortname = path1.stem
+                start_assistant(shortname.replace(".py", ""))
+    else:
+        print("⚠️Assistant Not Loaded⚠️")
+
+
+addon = os.environ.get("EXTRA_PLUGIN", None)
+
+
+async def addons():
+    if addon == "ON":
+        import glob
+
+        path = "userbot/plugins/Xtra_Plugin/*.py"
+        files = glob.glob(path)
+        for name in files:
+            with open(name) as f:
+                path1 = Path(f.name)
+                shortname = path1.stem
+                try:
+                    load_addons(shortname.replace(".py", ""))
+                except Exception as e:
+                    print(e)
+    else:
+        print("⚠️Addons Not Loading⚠️")
+
+
+abuse = os.environ.get("ABUSE", None)
+
+
+async def abuses():
+    if abuse == "ON":
+        import glob
+
+        path = "userbot/plugins/Abuse/*.py"
+        files = glob.glob(path)
+        for name in files:
+            with open(name) as f:
+                path1 = Path(f.name)
+                shortname = path1.stem
+                load_abuse(shortname.replace(".py", ""))
+    else:
+        print("⚠️Abuse Not Loading⚠️")
+
+
+spam = os.environ.get("SPAM", None)
+
+
+async def spams():
+    if spam == "ON":
+        import glob
+
+        path = "userbot/plugins/Spam/*.py"
+        files = glob.glob(path)
+        for name in files:
+            with open(name) as f:
+                path1 = Path(f.name)
+                shortname = path1.stem
+                start_spam(shortname.replace(".py", ""))
+    else:
+        print("⚠️Spam Not Loading⚠️")
+
+
+bot.loop.run_until_complete(module())
+bot.loop.run_until_complete(addons())
+bot.loop.run_until_complete(abuses())
+bot.loop.run_until_complete(assistants())
+bot.loop.run_until_complete(spams())
+
+print(
+    f"""
+╔════❰ZIDDIQUEEN❱═❍⊱❁۪۪
+║┣⪼ OWNER - {Config.ALIVE_NAME}
+║┣⪼ Group - @ziddiqueen_Userbot
+║┣⪼ CREATOR - @divya_9955
+║┣⪼ ZIDDIQUEEN - {LEGENDversion}
+║┣⪼ ✨ 『🔱 ZIDDIQUEEN 🔱』𝐔𝐬𝐞𝐫𝐛𝐨𝐭✨
+║╰━━━━━━━━━━━━━━━➣
+╚══════════════════❍⊱"""
+)
+print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
+
+
+
+if len(sys.argv) not in (1, 3, 4):
+    bot.disconnect()
+else:
+    bot.tgbot = None
+    bot.run_until_disconnected()
